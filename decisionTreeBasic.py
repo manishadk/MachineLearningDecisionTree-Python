0d41b8adb394a00ee9e47542383fed7ed52b9901
manishadk <manishadhikari777@gmail.com>
2016-09-08 20:49:50 +0545

    Basic Decision Tree

--- decisionTreeBasic.py
+++ decisionTreeBasic.py
@@ -1,16 +1,17 @@
 from sklearn import tree
  # scikitlearn module/library
- # classifier(box of rules) used here is tree
+ # tree is the classifier(box of rules) used 
 
-# training data qunatified(ints instead of strings)
+# training data qunatified(ints instead of strings-say xyz=1 and mno=2)
 features =[[150,1,2],[170,0,2],[130,1,1],[180,0,1]]
 labels = [0,1,0,1]
 
 classifier=tree.DecisionTreeClassifier() # empty classifier
+
 # now we must train the classifier through learning
 # we learn by finding patterns in our training data
-
 classifier = classifier.fit(features, labels)
+
 #use predict to classify the new feature/data
 print(classifier.predict([[190,0,0]]))
 
